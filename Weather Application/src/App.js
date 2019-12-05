6786c163627970a04fe9e06c249d509e2a5dd00c
Mehwish Roohan <53234237+MishiCodes@users.noreply.github.com>
2019-12-05 20:16:14 +0100

    Add files via upload

---
+++ Weather Application/src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+const API_KEY = "24d176143bf0e81176906d7c1366eced";
+
+class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault(); //prevent the page from reloading(default behaviour) when we press button
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    if (city && country) {
+      const api_call = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+      );
+      const data = await api_call.json();
+
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter any value"
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            {/*<div className="container">
+              <div className="row">*/}
+            <div className="col-xs-5 title-container">
+              <Titles />
+            </div>
+            <div className="col-xs-7 form-container">
+              <Form getWeather={this.getWeather} />
+              <Weather
+                temperature={this.state.temperature}
+                city={this.state.city}
+                country={this.state.country}
+                humidity={this.state.humidity}
+                description={this.state.description}
+                error={this.state.error}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
