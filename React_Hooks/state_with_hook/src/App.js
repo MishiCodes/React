45b81992699b81d0d283d55654a334ddbe74404b
Mehwish Roohan <53234237+MishiCodes@users.noreply.github.com>
2019-12-28 07:24:53 +0100

    Add files via upload

---
+++ React_Hooks/state_with_hook/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+//import ClassCounter from './components/ClassCounter';
+//import HookCounter from './components/HookCounter';
+//import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterFive from './components/HookCounterFive';
+
+function App() {
+  return (
+    <div className="App">
+      {/* {<ClassCounter /> */}
+      {/*<HookCounter /> */}
+      {/*<HookCounterTwo />*/}
+      <HookCounterFive />
+    </div>
+  );
+}
+
+export default App;
